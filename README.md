0a54fda94111f7118259e72ccc0ad0e8db50eda7
Adrian Mörchen <gitlab.euve@rest.scrobble.me>
2014-12-02 22:42:10 +0100

    Version 1.1 to be compatible with latest "WooCommerce Print Invoice & Deliveriy Note"

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WooCommerce Apply Order Discount
+# WooCommerce Print Credit Notes
 
 This plugin allows administrators to print credit notes, when an order was canceled.
 
@@ -14,4 +14,14 @@ If you want to support this plugin, you can
     * Contribute to [https://github.com/woothemes/woocommerce/issues/3164](https://github.com/woothemes/woocommerce/issues/3164)
     * Contribute to [https://github.com/piffpaffpuff/woocommerce-delivery-notes](https://github.com/piffpaffpuff/woocommerce-delivery-notes)
 * [Purchase](http://codecanyon.net/user/scrobbleme/portfolio?ref=scrobbleme) my work at Envato
-* [Hire me](http://www.scrobble.me) for a Wordpress job
+* [Hire me](http://blog.scrobble.me) for a WordPress job
+
+## Changelog
+
+### 1.1
+
+* Compatibility with WooCommerce Print Invoice & Delivery Note 4.0.2
+
+### 1.0
+
+* Initial commit
--- woocommerce-print-credit-notes.php
+++ woocommerce-print-credit-notes.php
@@ -5,19 +5,20 @@
  * Description: This plugin provides a simple way to print credit notes. It requires the plugin "WooCommerce Print Invoices & Delivery Notes" to work.
  * Author: Adrian Moerchen
  * Author URI: http://www.scrobble.me
- * Version: 1.0
+ * Version: 1.1
  *
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 
-add_action('after_switch_theme', 'wpcn_table_install');
+register_activation_hook(__FILE__, 'wpcn_table_install');
+add_action('woocommerce_init', 'wpcn_add_template_type', 100);
 add_action('woocommerce_admin_order_actions_end', 'wpcn_add_listing_actions', 100);
-add_filter('woocommerce_locate_template', 'wpcn_locate_template', 10, 3);
+add_filter('wcdn_document_title', 'wpcn_get_document_title', 100);
+add_filter('wcdn_order_info_fields', 'wpcn_get_order_info_fields', 1, 2);
 
-function wpcn_table_install()
-{
+function wpcn_table_install() {
     global $wpdb;
     $table_name = $wpdb->prefix . "woocommerce_credit_note";
     $sql = 'CREATE TABLE ' . $table_name . ' ('
@@ -33,8 +34,7 @@ function wpcn_table_install()
     add_option("wccn_db_version", '1.0');
 }
 
-function wccn_get_credit_note_number_and_date($order_id)
-{
+function wpcn_get_credit_note_number_and_date($order_id) {
     global $wpdb;
     $table_name = $wpdb->prefix . "woocommerce_credit_note";
     $credit_note_number = $wpdb->get_row($wpdb->prepare('SELECT credit_note_number, date FROM ' . $table_name . ' WHERE order_id = %d;', $order_id));
@@ -46,35 +46,49 @@ function wccn_get_credit_note_number_and_date($order_id)
             ),
             array('%d')
         );
-        return wccn_get_credit_note_number_and_date($order_id);
+        return wpcn_get_credit_note_number_and_date($order_id);
     }
     return $credit_note_number;
 }
 
-function wpcn_add_listing_actions($order)
-{
+function wpcn_add_template_type() {
+    global $wcdn;
+    $wcdn->print->template_types = array_merge($wcdn->print->template_types, array('credit-note'));
+}
+
+function wpcn_add_listing_actions($order) {
+
     if ($order->status != 'refunded' && $order->status != 'cancelled') {
         return;
     }
     ?>
-    <a href="<?php echo wp_nonce_url(admin_url('admin-ajax.php?action=generate_print_content&template_type=credit-note&order_id=' . $order->id), 'generate_print_content'); ?>"
-       class="button tips print-preview-button" target="_blank"
-       title="<?php esc_attr_e('Print Credit Note', 'woocommerce-print-credit-notes'); ?>"
+
+    <a href="<?php echo wcdn_get_print_link($order->id, 'credit-note'); ?>"
+       class="button tips print-preview-button credit-note" target="_blank"
+       alt="<?php esc_attr_e('Print Credit Note', 'woocommerce-print-credit-notes'); ?>"
        data-tip="<?php esc_attr_e('Print Credit Note', 'woocommerce-print-credit-notes'); ?>">
         <span><?php _e('Print Credit Note', 'woocommerce-print-credit-notes'); ?></span>
-        <img
-            src="<?php echo plugin_dir_url(__FILE__) . 'print-credit-note.png' ?>"
-            alt="<?php esc_attr_e('Print Credit Note', 'woocommerce-print-credit-notes'); ?>" width="14">
+        <img style="width: 18px; padding-top: 3px; padding-left: 1px;"
+             src="<?php echo plugin_dir_url(__FILE__) . 'print-credit-note.png' ?>"
+             alt="<?php esc_attr_e('Print Credit Note', 'woocommerce-print-credit-notes'); ?>" width="14">
     </a>
 <?php
 }
 
-function wpcn_locate_template($template, $template_name, $template_path)
-{
-    if ($template_name == 'print-credit-note.php') {
-        return plugin_dir_path(__FILE__) . 'templates/print-credit-note.php';
+function wpcn_get_document_title($title) {
+    if (wcdn_get_template_type() == 'credit-note') {
+        return __('Credit Note', 'woocommerce-print-credit-notes');
     }
-    return $template;
+    return $title;
 }
 
-
+function wpcn_get_order_info_fields($fields, $order) {
+    $credit_note_number = wpcn_get_credit_note_number_and_date($order->id);
+    if (wcdn_get_template_type() == 'credit-note') {
+        $fields = array_merge(array(
+            array("label" => __('Credit Note Nr.', 'woocommerce-print-credit-notes'), "value" => '#' . $credit_note_number->credit_note_number),
+            array("label" => __('Credit Note Date', 'woocommerce-print-credit-notes'), "value" => date_i18n(get_option('date_format'), strtotime($credit_note_number->date)))
+        ), $fields);
+    }
+    return $fields;
+}
